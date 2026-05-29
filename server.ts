8eb62ff2fbbeb527bbd8fcd06e01b5a61406b7fa
Eng-Bode2004 <[bodeashrafahmed@gmail.com](mailto:bodeashrafahmed@gmail.com)>
2026-05-30 02:18:57 +0300

    init commit

--- server.ts
+++ server.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/api/v1/en/users", UserRoutes);
 
 // Start Server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on port ${PORT}`);
