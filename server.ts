19521a16118c5cd56a78d7a6da8071fef0b0d255
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2026-06-18 06:13:37 +0300

    start

--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import "./Config/DataBase.ts";
 import RoleRoutes from "./Routes/RoleRoutes.ts";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/roles", RoleRoutes);
