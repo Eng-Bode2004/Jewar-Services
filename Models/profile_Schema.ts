7523ad44d22fe495048d08947dca16d61a6da22f
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2026-06-21 13:07:48 +0200

    init commit

--- Models/profile_Schema.ts
+++ Models/profile_Schema.ts
@@ -34,7 +34,7 @@ const CustomerProfileSchema = new mongoose.Schema(
     status: {
       type: String,
       enum: ["active", "inactive"],
-      default: "active",
+      default: "inactive",
     },
   },
   { timestamps: true }
